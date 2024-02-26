20c94a7432548a38411627bca23373e147e5971c
josorian <josejaviersoriano54@gmail.com>
2024-02-27 00:47:59 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dessertclicket/data/DessertUiState.kt
@@ -0,0 +1,11 @@
+package com.example.dessertclicket.data
+import androidx.annotation.DrawableRes
+import com.example.dessertclicket.data.Datasource.dessertList
+
+data class DessertUiState(
+    val currentDessertIndex: Int = 0,
+    val dessertsSold: Int = 0,
+    val revenue: Int = 0,
+    val currentDessertPrice: Int = dessertList[currentDessertIndex].price,
+    @DrawableRes val currentDessertImageId: Int = dessertList[currentDessertIndex].imageId
+)
